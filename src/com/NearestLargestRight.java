a62ff934e7f6695608239d027bca0548eea1973e
Priyanka Singh <raopriyankasingh94@gmail.com>
2020-10-15 23:29:26 +0530

    Nearest Largest Smallest

---
+++ src/com/NearestLargestRight.java
@@ -0,0 +1,28 @@
+package com;
+
+import java.util.Stack;
+import java.util.Vector;
+
+public class NearestLargestRight {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 3, 2, 1, 6, 5, 8, 7, 9, 10 };
+		Stack<Integer> st = new Stack<Integer>();
+		Vector<Integer> v = new Vector<Integer>();
+		int len = arr.length;
+
+		for (int i = len - 1; i >= 0; i--) {
+			while (!st.empty() && st.peek() < arr[i]) {
+				st.pop();
+			}
+			if (st.empty()) {
+				v.add(-1);
+			} else {	
+				v.add(st.peek());
+			}
+			st.push(arr[i]);
+		}
+		System.out.println(v);
+	}
+}
